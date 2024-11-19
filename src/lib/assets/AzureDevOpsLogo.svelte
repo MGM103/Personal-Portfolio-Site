469f48feef716e8a64bac539fa73ca2c069ceb35
MGM103 <marcus.marinelli3@gmail.com>
2024-11-19 17:39:55 +1100

    Adding software skills

---
+++ src/lib/assets/AzureDevOpsLogo.svelte
@@ -0,0 +1,21 @@
+<svg
+	width="800px"
+	height="800px"
+	viewBox="0 0 16 16"
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+>
+	<title>Azure DevOps</title>
+	<path
+		fill="currentColor"
+		d="M15 3.622v8.512L11.5 15l-5.425-1.975v1.958L3.004 10.97l8.951.7V4.005L15 3.622zm-2.984.428L6.994 1v2.001L2.382 4.356 1 6.13v4.029l1.978.873V5.869l9.038-1.818z"
+	/></svg
+>
+
+<style scoped lang="scss">
+	svg {
+		color: var(--secondary);
+		max-height: 30px;
+		max-width: 30px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,36 @@
 <script>
 	import { BioImg, GitHubSvg, LinkedInSvg, ResumeSvg } from '$lib/assets';
+	import {
+		AngularSvg,
+		AzureCloudSvg,
+		AzureDevOpsSvg,
+		ClangSvg,
+		CSharpSvg,
+		CSSSvg,
+		DockerSvg,
+		GitHubActionsSvg,
+		GolangSvg,
+		HTML5Svg,
+		JavaScriptSvg,
+		JavaSvg,
+		JenkinsSvg,
+		KubernetesSvg,
+		MongodbSvg,
+		NextSvg,
+		NginxSvg,
+		NodeSvg,
+		PostgresSvg,
+		PysparkSvg,
+		PythonSvg,
+		ReactSvg,
+		RustSvg,
+		SoliditySvg,
+		SqliteSvg,
+		SqlSvg,
+		SvelteSvg,
+		TypeScriptSvg,
+		VueSvg
+	} from '$lib/assets';
 </script>
 
 <div class="home-container">
@@ -34,6 +65,53 @@
 			</div>
 		</div>
 	</div>
+	<div class="content-card software-skills">
+		<h3>Software Skill-set</h3>
+		<h4>Programming Languages</h4>
+		<div id="programming-languages">
+			<div class="row">
+				<GolangSvg />
+				<JavaScriptSvg />
+				<TypeScriptSvg />
+				<JavaSvg />
+				<SoliditySvg />
+			</div>
+			<div class="row">
+				<HTML5Svg />
+				<CSSSvg />
+				<ClangSvg />
+				<CSharpSvg />
+				<RustSvg />
+			</div>
+		</div>
+		<h4>JS Front-end Frameworks & Runtimes</h4>
+		<div class="row">
+			<ReactSvg />
+			<VueSvg />
+			<SvelteSvg />
+			<NextSvg />
+			<AngularSvg />
+			<NodeSvg />
+		</div>
+		<h4>Cloud & DevOps</h4>
+		<div class="row">
+			<AzureCloudSvg />
+			<AzureDevOpsSvg />
+			<DockerSvg />
+			<GitHubActionsSvg />
+			<JenkinsSvg />
+			<KubernetesSvg />
+			<NginxSvg />
+		</div>
+		<h4>Data</h4>
+		<div class="row">
+			<PostgresSvg />
+			<SqlSvg />
+			<SqliteSvg />
+			<MongodbSvg />
+			<PysparkSvg />
+		</div>
+	</div>
 </div>
 
 <style scoped lang="scss">
@@ -45,7 +123,9 @@
 		background-color: var(--surface-2);
 		box-shadow: 0 0px 2px var(--shadow-1);
 		border-radius: 8px;
+		margin-block: 1rem;
 		padding: 2rem;
+		width: fit-content;
 	}
 
 	.bio-top-row {
@@ -105,5 +185,30 @@
 		border-radius: 16px;
 		max-height: 250px;
 		max-width: 250px;
+	}
+
+	.software-skills {
+		h3 {
+			margin-top: 0;
+		}
+
+		h4 {
+			font-size: 0.875rem;
+			margin-top: 2rem;
+		}
+
+		#programming-languages {
+			display: flex;
+			flex-direction: column;
+			gap: 1rem;
+		}
+
+		.row {
+			align-items: center;
+			display: flex;
+			flex-wrap: wrap;
+			gap: 0.75rem;
+			justify-content: left;
+		}
 	}
 </style>
