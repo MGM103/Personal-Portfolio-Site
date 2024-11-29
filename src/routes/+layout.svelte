63336de323e8bdc37332e38c473f8bce1ec33803
MGM103 <marcus.marinelli3@gmail.com>
2024-11-29 17:49:34 +1100

    fix assignment to const var

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 <svelte:head>
 	<script>
 		if (document) {
-			const theme = localStorage.getItem('theme');
+			let theme = localStorage.getItem('theme');
 
 			if (!theme) {
 				theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
